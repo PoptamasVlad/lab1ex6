392ff014da48afb879e9d9bbcffd17070922fa76
Poptamas Vlad <poptamasvlad@gmail.com>
2022-11-25 20:41:49 +0200

    Add project files.

---
+++ lab1ex6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lab1ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Se citesc trei numere de la tastatura, x,y,z. Scrieti un program care va afisa cele trei valori in ordine
+            descrescatoare.
+                • Exemplu: citim 3,9,0 Afisam : 9 3 0*/
+
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+
+            if(x >= y && y >= z)
+            {
+                Console.WriteLine(x + " " + y + " " + z);
+            }
+            else if (x >= z && z >= y)
+            {
+                Console.WriteLine(x + " " + z + " " + y);
+            }
+            else if (y >= x && x >= z)
+            {
+                Console.WriteLine(y + " " + x + " " + z);
+            }
+            else if (y >= z && z >= x)
+            {
+                Console.WriteLine(y + " " + z + " " + x);
+            }
+            else if (z >= y && y >= x)
+            {
+                Console.WriteLine(z + " " + y + " " + x);
+            }
+            else
+            {
+                Console.WriteLine(z + " " + x + " " + y);
+            }
+        }
+    }
+}
